31dde008674d55e98cafa5817ee605235bd9e22c
Raul Harlev <raul.harlev@gmail.com>
2026-05-27 23:57:56 +0200

    fixing types

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPPORTED_LOCALES?: string
   readonly VITE_LANDING_PAGE?: string
   readonly VITE_API_URL?: string
+  readonly VITE_COMPANY_LINKEDIN_URL?: string
 }
 
 interface ImportMeta {
